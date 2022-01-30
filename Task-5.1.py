44ffb7ee118ce033b2174f98a81370b6324bac96
Dmitry Slapygin <abashot@yandex.ru>
2022-01-30 16:23:30 +0300

    Задача №1 выполнена

---
+++ Task-5.1.py
@@ -0,0 +1,57 @@
+documents = [
+        {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+        {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+        {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+        ]
+
+directories = {
+        '1': ['2207 876234', '11-2', '5455 028765'],
+        '2': ['10006'],
+        '3': []
+        }
+
+def doc_owner(number):
+    result = 'Документ не обнаружен'
+    for doc in documents:
+        if doc['number'] == number:
+            result = f'Владелец документа: {doc["name"]}'
+    return result
+
+def doc_shelf(number):
+    result = 'Документ не обнаружен'
+    for shelf_number, docs in directories.items():
+        for doc_number in docs:
+            if doc_number == number:
+                result = f'Номер полки: {shelf_number}'
+    return result
+
+def doc_app(number, doc_type, name, shelf):
+    if shelf in directories.keys():
+        documents.append({"type": doc_type, "number": number, "name": name})
+        directories[shelf].append(number)
+        print('Документ успешно добавлен!')
+    else:
+        print('Такой полки не существует')
+
+
+while True:
+    user_command = input('Введите команду (p,s,l,a): ')
+
+    if user_command == 'p':
+        user_input = input('Введите номер документа: ')
+        print(doc_owner(user_input))
+    elif user_command == 's':
+        user_input = input('Введите номер документа: ')
+        print(doc_shelf(user_input))
+    elif user_command == 'l':
+        print('Список всех документов: ')
+        for doc in documents:
+            print(f'{doc["type"]} "{doc["number"]}" "{doc["name"]}"')
+    elif user_command == 'a':
+        number = input('Введите номер документа: ')
+        doc_type = input('Введите тип документа: ')
+        name = input('Введите имя владельца: ')
+        shelf = input('Введите номер полки: ')
+        doc_app(number, doc_type, name, shelf)
+    else:
+        print('Недопустимая команда')
