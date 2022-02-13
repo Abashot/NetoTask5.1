24aba23957daa09193f05aff7b9bb6d1cd495ec0
Dmitry Slapygin <abashot@yandex.ru>
2022-02-13 19:35:55 +0300

    'move' and 'add shelf' command added

--- Task-5.1.py
+++ Task-5.1.py
@@ -10,22 +10,26 @@ directories = {
         '3': []
         }
 
-def doc_owner(number):
+print('Список команд:\np - поиск владельца по номеру документа\n\
+s - поиск полки по номеру документа\nl - вывести список всех документов\na - добавить новый документ\nd - удалить документ\n\
+m - переместить документ\nas - добавить новую полку\nq - выйти из программы')
+
+def show_doc_owner(number):
     result = 'Документ не обнаружен'
     for doc in documents:
         if doc['number'] == number:
             result = f'Владелец документа: {doc["name"]}'
     return result
 
-def doc_shelf(number):
-    result = 'Документ не обнаружен'
+def show_doc_shelf(number):
+    result ='Документ не обнаружен'
     for shelf_number, docs in directories.items():
         for doc_number in docs:
             if doc_number == number:
                 result = f'Номер полки: {shelf_number}'
     return result
 
-def doc_app(number, doc_type, name, shelf):
+def append_doc(number, doc_type, name, shelf):
     if shelf in directories.keys():
         documents.append({"type": doc_type, "number": number, "name": name})
         directories[shelf].append(number)
@@ -33,27 +37,51 @@ def doc_app(number, doc_type, name, shelf):
     else:
         print('Такой полки не существует')
 
-def doc_del(number):
+def delite_doc(number):
     for doc in documents:
         if doc['number'] == number:
             for i in range(len(documents)):
                 if documents[i].get('number') == number:
                     del documents[i]
-                    print('Документ удален')
-                    return
+                    for val in directories.values():
+                        val.remove(number)
+                        print('Документ удален')
+                        return
     else:
         print('Документ не обнаружен')
+
+def move_doc(number, shelf):
+    for doc in documents:
+        if doc['number'] == number:
+            if shelf in directories.keys():
+                for val in directories.values():
+                    val.remove(number)
+                    directories[shelf].append(number)
+                    print('Документ перемещен')
+                    return
+            else:
+                print('Такой полки не существует')
+                return
+    else:
+        print('Документ не обнаружен')
+
+def add_shelf(shelf):
+    if shelf in directories.keys():
+        print('Такая полка уже есть')
+    else:
+        directories[shelf] = []
+        print('Полка успешно добавлена')
         return
 
 while True:
-    user_command = input('Введите команду (p,s,l,a,d): ')
+    user_command = str.lower(input('\nВведите команду: '))
 
     if user_command == 'p':
         user_input = input('Введите номер документа: ')
-        print(doc_owner(user_input))
+        print(show_doc_owner(user_input))
     elif user_command == 's':
         user_input = input('Введите номер документа: ')
-        print(doc_shelf(user_input))
+        print(show_doc_shelf(user_input))
     elif user_command == 'l':
         print('Список всех документов: ')
         for doc in documents:
@@ -63,9 +91,19 @@ while True:
         doc_type = input('Введите тип документа: ')
         name = input('Введите имя владельца: ')
         shelf = input('Введите номер полки: ')
-        doc_app(number, doc_type, name, shelf)
+        append_doc(number, doc_type, name, shelf)
     elif user_command == 'd':
         number = input('Введите номер документа: ')
-        doc_del(number)
+        delite_doc(number)
+    elif user_command == 'm':
+        number = input('Введите номер документа: ')
+        shelf = input('Введите номер полки: ')
+        move_doc(number, shelf)
+    elif user_command == 'as':
+        shelf = input('Введите номер полки: ')
+        add_shelf(shelf)
+    elif user_command == 'q':
+        print('Программа закрыта')
+        break
     else:
-        print('Недопустимая команда')
+        print('Недопустимая команда!')
